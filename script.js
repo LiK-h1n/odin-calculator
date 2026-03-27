060e10e5a2a18713ae205d479f1c23502f8e76a3
LiK-h1n <nikhil.23.idiculla@gmail.com>
2026-03-27 12:34:43 +0530

    Add function operate

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
+function operate(operator, number1, number2) {
+    if (operator === "+") {
+        add(number1, number2);
+    } else if (operator === "-") {
+        subtract(number1, number2);
+    } else if (operator === "*") {
+        multiply(number1, number2);
+    } else if (operator === "/") {
+        divide(number1, number2);
+    }
+}
+
 let number1;
 let operator;
 let number2;
