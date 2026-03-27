bef976325666bf223ea00bc65c20b5d919daaf4f
LiK-h1n <nikhil.23.idiculla@gmail.com>
2026-03-27 18:26:21 +0530

    Add updateNumber, updateDisplay functions and added an eventListener for numerical buttons

--- script.js
+++ script.js
@@ -15,17 +15,56 @@ function divide(number1, number2) {
 }
 
 function operate(operator, number1, number2) {
-    if (operator === "+") {
-        add(number1, number2);
-    } else if (operator === "-") {
-        subtract(number1, number2);
-    } else if (operator === "*") {
-        multiply(number1, number2);
-    } else if (operator === "/") {
-        divide(number1, number2);
-    }
+  if (operator === "+") {
+    add(number1, number2);
+  } else if (operator === "-") {
+    subtract(number1, number2);
+  } else if (operator === "*") {
+    multiply(number1, number2);
+  } else if (operator === "/") {
+    divide(number1, number2);
+  }
 }
 
-let number1;
+let number1 = 0;
 let operator;
-let number2;
+let number2 = 0;
+
+function updateNumber(selection, value) {
+  switch (selection) {
+    case 1:
+      number1 *= 10;
+      number1 += value;
+      break;
+    case 2:
+      number2 *= 10;
+      number2 += value;
+  }
+}
+
+function updateDisplay(value) {
+  display.textContent = value;
+}
+
+const container = document.querySelector("#container");
+const display = document.querySelector("#display");
+
+container.addEventListener("click", (event) => {
+  const buttonID = event.target.id;
+
+  if (
+    buttonID === "0" ||
+    buttonID === "1" ||
+    buttonID === "2" ||
+    buttonID === "3" ||
+    buttonID === "4" ||
+    buttonID === "5" ||
+    buttonID === "6" ||
+    buttonID === "7" ||
+    buttonID === "8" ||
+    buttonID === "9"
+  ) {
+    updateNumber(1, Number(buttonID));
+    updateDisplay(number1);
+  }
+});
