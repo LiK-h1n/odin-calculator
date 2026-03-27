3ad9ff5053c0a889ede0efe88836c32b51211b3d
LiK-h1n <nikhil.23.idiculla@gmail.com>
2026-03-27 12:31:42 +0530

    Add variables number1, operator and number2

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function add(number1, number2) {
-    return number1 + number2;
+  return number1 + number2;
 }
 
 function subtract(number1, number2) {
-    return number1 - number2;
+  return number1 - number2;
 }
 
 function multiply(number1, number2) {
-    return number1 * number2;
+  return number1 * number2;
 }
 
 function divide(number1, number2) {
-    return number1 / number2;
+  return number1 / number2;
 }
+
+let number1;
+let operator;
+let number2;
