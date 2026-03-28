45dcd6f70836703278147a05b929e6bd09fdcb2a
LiK-h1n <nikhil.23.idiculla@gmail.com>
2026-03-28 19:48:53 +0530

    Add main functionality of calculator

--- script.js
+++ script.js
@@ -16,35 +16,104 @@ function divide(number1, number2) {
 
 function operate(operator, number1, number2) {
   if (operator === "+") {
-    add(number1, number2);
+    return add(number1, number2);
   } else if (operator === "-") {
-    subtract(number1, number2);
+    return subtract(number1, number2);
   } else if (operator === "*") {
-    multiply(number1, number2);
+    return multiply(number1, number2);
   } else if (operator === "/") {
-    divide(number1, number2);
+    return divide(number1, number2);
   }
 }
 
-let number1 = 0;
-let operator;
-let number2 = 0;
+function updateNumber(value) {
+  number = operate("*", number, 10);
+  number = operate("+", number, value);
+}
 
-function updateNumber(selection, value) {
-  switch (selection) {
-    case 1:
-      number1 *= 10;
-      number1 += value;
-      break;
-    case 2:
-      number2 *= 10;
-      number2 += value;
-  }
+function storeNumber() {
+  result = number;
+}
+
+function resetNumber() {
+  number = 0;
+}
+
+function resetOperator() {
+  operator = "";
+}
+
+function resetResult() {
+  result = 0;
 }
 
 function updateDisplay(value) {
   display.textContent = value;
 }
+
+function isResultFormatted() {
+  let resultString = String(result);
+
+  if (resultString.includes(".")) {
+    let partAfterDecimal = resultString.split(".")[1];
+
+    if (partAfterDecimal.length > 2) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function roundResult() {
+  result = Math.round(result * 100) / 100;
+}
+
+function divideByZero() {
+  return "( ｡ •̀ ᴖ •́ ｡)💢";
+}
+
+function setOperator(buttonID) {
+  if (buttonID == "minus") {
+    return "-";
+  } else if (buttonID == "divide") {
+    return "/";
+  } else if (buttonID == "multiply") {
+    return "*";
+  } else if (buttonID == "plus") {
+    return "+";
+  }
+}
+
+function resetWasEqualTo() {
+  wasEqualTo = false;
+}
+
+function setWasEqualTo() {
+  wasEqualTo = true;
+}
+
+function setWasNumberAssigned() {
+  wasNumberAssigned = true;
+}
+
+function resetWasNumberAssigned() {
+  wasNumberAssigned = false;
+}
+
+function backspaceNumber() {
+  number = Math.floor(operate("/", number, 10));
+}
+
+function backspaceResult() {
+  result = Math.floor(operate("/", result, 10));
+}
+
+let result = 0;
+let operator = "";
+let number = 0;
+let wasEqualTo = false;
+let wasNumberAssigned = false;
 
 const container = document.querySelector("#container");
 const display = document.querySelector("#display");
@@ -64,7 +133,102 @@ container.addEventListener("click", (event) => {
     buttonID === "8" ||
     buttonID === "9"
   ) {
-    updateNumber(1, Number(buttonID));
-    updateDisplay(number1);
+    if (wasEqualTo) {
+      resetNumber();
+      resetWasNumberAssigned();
+      resetResult();
+      resetOperator();
+      updateNumber(Number(buttonID));
+      updateDisplay(number);
+    } else {
+      if (!wasNumberAssigned) {
+        setWasNumberAssigned();
+      }
+
+      updateNumber(Number(buttonID));
+      updateDisplay(number);
+    }
+
+    resetWasEqualTo();
+  } else if (buttonID === "equal") {
+    if (operator !== "") {
+      if (operator === "/" && number === 0) {
+        resetNumber();
+        resetWasNumberAssigned();
+        resetResult();
+        resetOperator();
+        updateDisplay(divideByZero());
+        resetWasEqualTo();
+      } else {
+        result = operate(operator, result, number);
+
+        if (!isResultFormatted()) {
+          roundResult();
+        }
+
+        updateDisplay(result);
+        setWasEqualTo();
+      }
+    }
+  } else if (buttonID === "clear") {
+    resetNumber();
+    resetWasNumberAssigned();
+    resetResult();
+    resetOperator();
+    updateDisplay(result);
+    resetWasEqualTo();
+  } else if (buttonID === "clear-entry") {
+    resetNumber();
+    resetWasNumberAssigned();
+    updateDisplay(number);
+    resetWasEqualTo();
+  } else if (buttonID === "backspace") {
+      if (wasEqualTo) {
+        backspaceResult();
+        updateDisplay(result);
+      } else {
+        backspaceNumber();
+        updateDisplay(number);
+      }
+  } else if (buttonID === "minus" || buttonID === "divide" || buttonID === "multiply" || buttonID === "plus"){
+    if (operator !== "") {
+      if (wasNumberAssigned) {
+        if (!wasEqualTo) {
+          if (operator === "/" && number === 0) {
+            resetNumber();
+            resetWasNumberAssigned();
+            resetResult();
+            resetOperator();
+            updateDisplay(divideByZero());
+            resetWasEqualTo();
+          } else {
+            result = operate(operator, result, number);
+
+            if (!isResultFormatted()) {
+              roundResult();
+            }
+
+            updateDisplay(result);
+            resetNumber();
+            resetWasNumberAssigned();
+            resetWasEqualTo();
+            operator = setOperator(buttonID);
+          }
+        } else {
+          resetNumber();
+          resetWasNumberAssigned();
+          resetWasEqualTo();
+          operator = setOperator(buttonID);
+        }
+      }
+      else {
+        operator = setOperator(buttonID);
+      }
+    } else {
+      storeNumber();
+      resetNumber();
+      resetWasNumberAssigned();
+      operator = setOperator(buttonID);
+    }
   }
 });
